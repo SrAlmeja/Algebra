a79e06b27a5c3e25d78ae87a3ae868833de0ec2f
Angel Legorreta <legosurio@hotmail.com>
2021-02-23 14:56:11 -0600

    Subí todo al repo

---
+++ Assets/Vector magnitud.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x;
+        float y;
+        float z;
+        float Magnitud;
+        float Normilized;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    double Magnitud(float x, float y, float z)
+    {
+        double respuesta = Mathf.Sqrt
+
+
+
+
+}
