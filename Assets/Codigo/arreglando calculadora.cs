7f01a7df9c103bc4b0c6e20aad38ad169b75ce0b
Angel Legorreta <legosurio@hotmail.com>
2021-03-23 20:00:21 -0600

    corrigiendo código

---
+++ Assets/Codigo/arreglando calculadora.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arreglandocalculadora : MonoBehaviour
+{
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int sum = suma(5, 8);
+        print("suma" + sum);
+
+        //Move();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+    }
+
+
+    public int suma(int a, int b)
+    {
+        int c = a + b;
+        return c;
+    }
+
+
+}
